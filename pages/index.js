eb98a546cd2bcfd9ab78e1fea577743a6b4b21e0
Gabriel Nunes <gnunesmail@gmail.com>
2022-01-20 19:06:22 -0300

    fix: ajustar componente desnecessário

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
-import Head from "next/head";
-
 import Layout from "../components/Layout";
 import SearchInput from "../components/SearchInput";
 
 export default function Home() {
   return (
     <Layout>
-      <Head />
       <SearchInput />
     </Layout>
   );
