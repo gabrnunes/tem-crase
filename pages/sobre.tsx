d259f77a4a7f98ce832bb1c2f1f68f4bac1a8486
Gabriel Nunes <gnunesmail@gmail.com>
2022-03-03 13:58:57 -0300

    fix: corrigir texto

--- pages/sobre.tsx
+++ pages/sobre.tsx
@@ -9,10 +9,10 @@ export default function Sobre() {
       <>
         <h1>A história do tem crase</h1>
         <p>
-          O site surgiu depois de inumeras pesquisas na internet do tipo &#8222;
-          volta as aulas tem crase?&#8222;. E encontrar respostas gigantes que
-          explicavam todas as regras da crase não respondiam apenas &#8222;NÃO!
-          &#8222;
+          O site surgiu depois de inumeras pesquisas na internet do tipo &quot;
+          volta as aulas tem crase?&quot;. E encontrar respostas gigantes que
+          explicavam todas as regras da crase não respondiam apenas &quot;NÃO!
+          &quot;
         </p>
         <p>
           A ideia inicial era construir uma inteligência artificial que aprende
@@ -65,7 +65,7 @@ export default function Sobre() {
               rel="noopener noreferrer"
               className="link"
             >
-              &#8222;Qual é o certo?&#8222;
+              &quot;Qual é o certo?&quot;
             </a>{' '}
             e ajudou em muitas respostas que temos aqui.
           </li>
@@ -90,7 +90,7 @@ export default function Sobre() {
             >
               Tânia d&apos;Arc
             </a>{' '}
-            que criou o texto da página &#8222;O que é crase?&#8222;
+            que criou o texto da página &quot;O que é crase?&quot;
           </li>
         </ul>
       </>
